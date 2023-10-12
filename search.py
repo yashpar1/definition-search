221abb79f637a310d57a27f68cf5457ffa519b9a
Yashwant Parmar <yashwant.parmar@gmail.com>
2023-10-12 14:03:15 -0700

    Added API call (and response parsing) for dictionary API

--- search.py
+++ search.py
@@ -1,2 +1,11 @@
-text = input('Paste the text you want to search here')
-term = input('Input the word you want to search for here')
+import requests
+
+base_uri = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
+
+text = input('Paste the text you want to search here: ')
+search_term = input('Input the word you want to search for here: ')
+
+search_uri = base_uri + search_term
+word = requests.get(search_uri).json()[0]
+definitions = []
+definitions += [d['definitions'][0]['definition'] for d in word['meanings']]

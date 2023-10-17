e8218ca0eab57d17706ac8381ba37258d72ce152
Yashwant Parmar <yashwant.parmar@gmail.com>
2023-10-16 18:48:54 -0700

    Refactored to increase code reusability

--- search.py
+++ search.py
@@ -10,14 +10,18 @@ base_uri = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
 text = input('Paste the text you want to search here: ')
 search_term = input('Input the word you want to search for here: ')
 
-search_uri = base_uri + search_term
-word = requests.get(search_uri).json()[0]
-definitions = []
+def get_defs(word):
+    search_uri = base_uri + word
+    data = requests.get(search_uri).json()[0]
+    # note-to-self I have the [0] because d['definitions'] returns a list of definitions
+    # currently just using the first; could add loop to get all of em if there are multiple
+    definitions = [d['definitions'][0]['definition'] for d in data['meanings']]
+    df_defs = pd.DataFrame(definitions, columns=['definitions'])
+    return df_defs
+
+
+df_defs = get_defs(search_term)
 sentences = sent_tokenize(text)
-# note-to-self I have the [0] because d['definitions'] returns a list of definitions
-# currently just using the first; could add loop to get all of em if there are multiple
-definitions += [d['definitions'][0]['definition'] for d in word['meanings']]
-df_defs = pd.DataFrame(definitions, columns=['definitions'])
 df_sents = pd.DataFrame(sentences, columns=['sentences'])
 
 porter = PorterStemmer()
@@ -25,9 +29,17 @@ def clean(sentence):
     punc = string.punctuation
     no_punc = str.maketrans('', '', punc)
     stops = set(stopwords.words('english'))
+
     tokens = word_tokenize(sentence)
-    words = [porter.stem(w.translate(no_punc)) for w in tokens if (w not in stops and w != '``' and w != "''" and w not in punc)]
-    return words
+    stemmer = lambda x : porter.stem(x.translate(no_punc))
+    stems = []
+    for w in tokens:
+        if (w not in stops and w != '``' and w != "''" and w != '’' and w not in punc):
+            # w_def = word_tokenize(get_defs(w))
+            stems += [stemmer(w)]
+            # stems += [stemmer(w_def)]
+
+    return stems
 
 df_defs['def_stems'] = df_defs['definitions'].apply(lambda row: clean(row))
 df_sents['text_stems'] = df_sents['sentences'].apply(lambda row: clean(row))
@@ -41,8 +53,10 @@ def vectorize(text, definition):
     cos_sim = cosine_similarity(matrix, matrix)
     return cos_sim[0, 1]
 
+df_sim = pd.DataFrame()
 for i in range(len(df_defs)):
-    df_sents[f'similarity_{i}'] = df_sents['text_stems'].apply(lambda row: vectorize(row, df_defs['def_stems'].loc[i]))
+    df_sim[f'similarity_{i}'] = df_sents['text_stems'].apply(lambda row: vectorize(row, df_defs['def_stems'].loc[i]))
     i += 1
 
+print(df_sim)
 print(df_sents)
